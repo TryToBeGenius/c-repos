817329b6b2007419b4ba6fea822261eeb6dba8d2
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-13 12:54:34 +0500

    homework3 done

---
+++ homework3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 < 10 || num1 > 99){
+    Console.WriteLine("Неверный ввод. Введите число от 10 до 99");
+}
+else{
+    if (num1 % 10 > num1 / 10){
+        Console.WriteLine(num1 % 10);
+    } 
+    else{
+        Console.WriteLine(num1 / 10);
+    }
+}св
